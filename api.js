68dec6b0ae572dcb79673116546598a7630d3ff0
Jack Bowman <jackbow@umich.edu>
2020-07-05 20:30:34 -0400

    fix api domain code

--- api.js
+++ api.js
@@ -1,6 +1,7 @@
 import axios from 'axios'
 
-const domain = 'https://transmission.um.city:5000'
+const devEnv = false
+const domain = devEnv ? 'http://127.0.0.1:5000' : ''
 
 export default {
   getShares: () => {
